27219283709d76b2e5e64fd431854c5b8da974a9
payhelpa <dev@payhelpa.com>
2024-04-24 07:17:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000; // or any port you prefer
+
+app.get('/tesla-locations', (req, res) => {
+  // Make sure to provide the correct path to your JSON file
+  fs.readFile('parsed_locations.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading the file:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    // Data is already in JSON format, so we can directly parse it
+    const jsonContent = JSON.parse(data);
+
+    // Set header to application/json
+    res.setHeader('Content-Type', 'application/json');
+
+    // Send the JSON content as the response
+    res.json(jsonContent);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
