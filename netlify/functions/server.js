e09c3e1adef586f676e528aeb24a17c6581458d2
payhelpa <dev@payhelpa.com>
2024-04-24 08:13:54 +0100

    netlify commit

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,4 +1,32 @@
-const app = require('../../app');
-const serverless = require('serverless-http');
 
+const serverless = require('serverless-http');
+const express = require('express');
+const fs = require('fs');
+const app = express(); // or any port you prefer
+
+app.get('/.netlify/functions/tesla-locations', (req, res) => {
+  // Make sure to provide the correct path to your JSON file
+  fs.readFile('parsed_locations.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading the file:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    // Data is already in JSON format, so we can directly parse it
+    const jsonContent = JSON.parse(data);
+
+    // Set header to application/json
+    res.setHeader('Content-Type', 'application/json');
+
+    // Send the JSON content as the response
+    res.json(jsonContent);
+  });
+});
+
+module.exports = app;
 module.exports.handler = serverless(app);
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
